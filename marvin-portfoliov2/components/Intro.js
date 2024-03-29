ba46df96a8d3c2d86108e93399e334e3a2b3c671
Marvin Xia <marvinxia09@gmail.com>
2024-03-28 21:07:59 -0700

    initial index page

---
+++ marvin-portfoliov2/components/Intro.js
@@ -0,0 +1,15 @@
+import React from "react";
+import { Box, Container, Flex} from "@chakra-ui/react";
+
+export default function Intro() {
+    return( 
+        <>
+            <Container>
+                <Box bg="gray" borderRadius="lg" textAlign="center" p="3">
+                    Hello, I am a full-stack developer based out in East Bay, CA
+                </Box>
+                Marvin Xia
+            </Container>
+        </>
+    )
+}
---
+++ marvin-portfoliov2/pages/index.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import { ChakraProvider } from '@chakra-ui/react'
+import Intro from '@/components/Intro';
+
+
+
+export default function Home() {
+  return (
+    <>
+      <ChakraProvider>
+        <Intro />
+        
+      </ChakraProvider>
+    </>
+  );
+}
