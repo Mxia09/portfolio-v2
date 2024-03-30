f3091ffa25f61597796f382a2183623b2ef04688
Marvin Xia <marvinxia09@gmail.com>
2024-03-29 18:10:29 -0700

    initial Nav Bar

--- marvin-portfoliov2/components/Nav.js
+++ marvin-portfoliov2/components/Nav.js
@@ -1,12 +1,30 @@
 import Link from "next/link";
 import React from "react";
-import { Center, Flex, Spacer } from '@chakra-ui/react'
+import styled from "styled-components";
+import { Center , Flex, Container, Stack, Box} from '@chakra-ui/react'
 import Logo from "./Logo";
+
+// const HorizontalWrapper = styled.div`
+//     display: flex;
+// `;
 
 export default function Nav() {
     return (
         <>
         <Logo />
+        <Container mt="-10">
+            <Stack direction={{ base: 'column', md: 'row' }}>
+                <Box>
+                    <Link href={'/'}>About</Link>
+                </Box>
+                <Box>
+                    <Link href={'/'}>Projects</Link>
+                </Box>
+                <Box>
+                    <Link href={'/'}>Contact</Link>
+                </Box>
+            </Stack>
+        </Container>
         </>
     )
 }
